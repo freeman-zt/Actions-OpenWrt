ce9bc87b4bb5198e039bc3191eba66746e843c80
freeman-zt <110078494+freeman-zt@users.noreply.github.com>
2025-08-05 19:24:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git OpenOray https://github.com/OrayOS/OpenOray.git' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
