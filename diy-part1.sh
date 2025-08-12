feb6c7c6d099f7a1cc275e75ef48c436cc59f20e
freeman-zt <110078494+freeman-zt@users.noreply.github.com>
2025-08-12 10:36:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git OpenOray https://github.com/OrayOS/OpenOray.git' >>feeds.conf.default
+echo 'src-git OpenOray https://github.com/OrayOS/OpenOray.git' >>feeds.conf.default
